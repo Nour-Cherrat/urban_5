7ad5510fa9d2f3890543b2f5fd914182ccfd6eb3
Nour-Cherrat <cherrat.nour@gmail.com>
2024-05-29 00:34:36 +0100

    Add logout

---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "config.php";
+
+// Start the session
+session_start();
+
+// Unset all of the session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to the login page or any other desired page
+header("Location: index.html");
+exit;
+?>
